1d8830b63bf311d2eb7676fa5ea5c9f26da623b3
Ossi Honkanen <ohonkanen@gmail.com>
2017-05-02 23:18:25 +0300

    My first commit, ever.

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var birthYear = 1979
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    @IBOutlet weak var todaysDate: UILabel!
+    
+    @IBAction func lifeSucksPressed(_ sender: Any) {
+        greetingLabel.text = "Yes."
+    }
+    
+    @IBAction func lifeIsGoodPressed(_ sender: Any) {
+        greetingLabel.text = "No. It sucks."
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if birthYear < 2000 {
+            print("You're born on the right side of the Millennium")
+        }   else {
+            print("2K GOGO!")
+        }
+        
+        todaysDate.text = "17.2.2022"
+        greetingLabel.text = "Mohsenator"
     }
 
     override func didReceiveMemoryWarning() {
